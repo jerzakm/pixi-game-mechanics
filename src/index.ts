0397b51f11cf6c9aaa5b2a784bd547415d348cf3
Marcin <jerzakm@gmail.com>
2019-08-30 07:40:34 +0200

    adds formation demo

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import "../src/_scss/main.scss"
 import * as renderer from './core/renderer'
 import * as Stats from 'stats.js'
 import { initShadowcasting } from "./01_fov/shadowcasting";
-import { initPhysicsDemo } from "./02_physics_with_matter/physicsDemo";
+import { initPhysicsDemo } from "./02_physics_with_matter/physicsSquares";
+import { initPhysicsBattleFormation } from "./02_physics_with_matter/physicsBattleFormation";
 
 renderer.initRenderer()
 
@@ -11,14 +12,17 @@ stats.showPanel(0)
 document.body.appendChild(stats.dom)
 
 // const shadowcasting = initShadowcasting(renderer.stage)
-const physicsDemo = initPhysicsDemo(renderer.stage)
+// const physicsDemo = initPhysicsDemo(renderer.stage)
+
+const physicsBattleFormation = initPhysicsBattleFormation(renderer.stage)
 
 
 renderer.ticker.add((delta: number) => {
     stats.begin()
 
     // shadowcasting()
-    physicsDemo(delta)
+    // physicsDemo(delta)
+    physicsBattleFormation(delta)
 
     stats.end()
 })
