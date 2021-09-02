1e4dab89001e8f130e7658e28689d4cfe6e9a03a
Marcin Jerzak <jerzakm@gmail.com>
2021-09-02 21:23:55 +0200

    - sveltekit - tailwind - typescript

--- src/math/polyMath.ts
+++
@@ -1,7 +0,0 @@
-export const calcHexPoints = (r: number,h: number, flat: boolean): number[] => {
-    if(flat) {
-        return [-r, 0,-r/2, h/2,r/2, h/2, r, 0, r/2, -h/2, -r/2, -h/2]
-    } else {
-        return [-h/2,r/2, 0, r, h/2, r/2, h/2, -r/2, 0, -r, -h/2, -r/2]
-    }
-}
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	// import '../styles/tailwind-output.css';
+	import '../styles.scss';
+	$: active = $page.path;
+</script>
+
+{active}
+<main>
+	<slot />
+</main>
