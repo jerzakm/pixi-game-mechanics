6318db7bc59660d2571c464374faa7f23dbe32e0
Marcin Jerzak <jerzakm@gmail.com>
2021-09-03 00:00:50 +0200

    - remove and ignore tailwind output - color palette in tailwind config and scss

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,5 +2,12 @@ module.exports = {
     // ...
     mode: 'jit', 
     purge: ["./src/**/*.svelte"],
+    theme: {
+      colors: {
+        primary: '#F68F8F',
+        dark: '#121212',
+        light: '#FAFAFA'
+      }
+    }
     // ...
   }
