0bb9b6d7c2f214811e283dfc1b966c5875a72217
Marcin Jerzak <jerzakm@gmail.com>
2021-09-03 22:41:33 +0200

    simple component to standardize article

---
+++ src/lib/Article.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let category: string = '';
+	export let title: string = '';
+</script>
+
+<h2 class="category">{category}</h2>
+<h1 class="title">{title}</h1>
+<slot />
+
+<style>
+	.category {
+		color: white;
+		@apply mt-4;
+		@apply font-medium;
+	}
+	.title {
+		@apply mb-2;
+	}
+</style>
