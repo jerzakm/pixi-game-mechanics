be29a3ddf1a948d50433a6e86da413f2d1cde88a
Marcin Jerzak <jerzakm@gmail.com>
2021-09-03 23:20:37 +0200

    Working Pixi component - basic resize responsiveness - resize buttons
    Exposes renderer directly and doesn't run the ticker, in each 'experiment - article'  I have to either write my own loop with requestAnimationFrame or PIXI.Ticker

--- src/lib/PixiView.svelte
+++ src/lib/PixiView.svelte
@@ -3,33 +3,86 @@
 	import * as PIXI from 'pixi.js';
 
 	let canvas: HTMLCanvasElement;
-	export let mainScene = new PIXI.Container();
-	export let step: Function;
-	export let app: PIXI.Application;
-	export let applicationOptions;
+	export let applicationOptions: PIXI.IRendererOptions;
+	export let renderer: PIXI.Renderer;
 
 	onMount(() => {
 		const options = Object.assign(
 			{
-				width: 800,
-				height: 600,
 				backgroundColor: null,
-				transparent: true,
 				resolution: window.devicePixelRatio || 1,
 				view: canvas
 			},
 			applicationOptions
 		);
 
-		app = new PIXI.Application(options);
+		renderer = new PIXI.Renderer(options);
 
-		app.stage.addChild(mainScene);
-
-		// Listen for animate update
-		app.ticker.add((delta) => {
-			if (step) step(delta);
+		resizeCanvas();
+		canvas.addEventListener('resize', () => {
+			resizeCanvas();
 		});
 	});
+
+	const resizeCanvas = () => {
+		const box = canvas.getBoundingClientRect();
+		renderer.resize(box.width, box.height);
+	};
+
+	const goFullscreen = (background = false) => {
+		canvas.style.position = 'fixed';
+		canvas.style.top = '0px';
+		canvas.style.left = '0px;';
+		canvas.style.width = `${window.innerWidth}px`;
+		canvas.style.height = `${window.innerHeight}px`;
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+		canvas.style.zIndex = `${background ? -100 : 100}`;
+	};
+
+	const goDefaultView = () => {
+		canvas.style.position = 'unset';
+		canvas.style.top = 'unset';
+		canvas.style.left = 'unset';
+		canvas.style.width = `100%`;
+		canvas.style.height = `100%`;
+		canvas.style.zIndex = `1`;
+		const box = canvas.getBoundingClientRect();
+		canvas.width = box.width;
+		canvas.height = box.height;
+	};
 </script>
 
-<canvas bind:this={canvas} />
+<pixiWrapper>
+	<canvasBox>
+		<canvas bind:this={canvas} />
+	</canvasBox>
+	<resizeButtons>
+		<b>Resize: </b>
+		<button on:click={() => goDefaultView()}>Small (default)</button>
+		<button on:click={() => goFullscreen()}>Fullscreen</button>
+		<button on:click={() => goFullscreen(true)}>Fullscreen Background</button>
+	</resizeButtons>
+</pixiWrapper>
+
+<style lang="postcss">
+	canvasBox {
+		max-width: 800px;
+		height: 500px;
+		display: block;
+	}
+	canvas {
+		width: 100%;
+		height: 100%;
+	}
+	pixiWrapper {
+		@apply flex flex-col;
+	}
+	button {
+		@apply ml-4 p-2 rounded-lg text-xs font-bold;
+	}
+	resizeButtons {
+		z-index: 99999;
+		@apply absolute flex items-center mt-4 ml-4;
+	}
+</style>
--- src/routes/other/pixiPreviewComponent/index.svelte
+++ src/routes/other/pixiPreviewComponent/index.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
 	import PixiView from '$lib/PixiView.svelte';
+	import Article from '$lib/Article.svelte';
 	import { onMount } from 'svelte';
 	import * as PIXI from 'pixi.js';
 
-	let app;
-	let mainScene;
-	let step;
+	let mainScene = new PIXI.Container();
+	let renderer: PIXI.Renderer;
 
-	let applicationOptions: PIXI.IApplicationOptions = {
-		backgroundColor: 0xffffff
+	let applicationOptions: PIXI.IRendererOptions = {
+		backgroundColor: 0x666666
 	};
 
 	const texture = PIXI.Texture.from('/assets/meeple.png');
@@ -17,20 +17,30 @@
 	meeple.anchor.set(0.5);
 
 	function start() {
-		console.log(app.renderer.options);
 		mainScene.addChild(meeple);
-
-		meeple.x = app.renderer.options.width / 2;
-		meeple.y = app.renderer.options.height / 2;
-
-		step = (delta: number) => {
-			meeple.rotation -= 0.01 * delta;
-		};
 	}
+
+	const fps = 60;
+	let lastFrame = 0;
+	const step = (time: number = 0) => {
+		const delta = time - lastFrame;
+		lastFrame = time;
+		meeple.x = renderer.options.width / 2;
+		meeple.y = renderer.options.height / 2;
+		meeple.rotation += 0.001 * delta;
+
+		renderer.render(mainScene);
+		requestAnimationFrame(step);
+	};
 
 	onMount(async () => {
 		await start();
+		step();
 	});
 </script>
 
-<PixiView bind:app bind:mainScene bind:step bind:applicationOptions />
+<PixiView bind:applicationOptions bind:renderer />
+
+<Article category={'Other'} title={'Pixi.js preview component'}>
+	<p>Simple Svelte component consisting of Pixi.JS powered canvas.</p></Article
+>
