e0382ff5d2142897b9b37ba06b2117a5d7ed350f
Marcin Jerzak <jerzakm@gmail.com>
2021-09-09 00:43:49 +0200

    - mdsvex basic setup

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,20 @@
 import preprocess from 'svelte-preprocess';
 
+import {mdsvex} from 'mdsvex'
+
+
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	extensions: ['.svelte', '.md'],
+	preprocess: [
+		preprocess(),
+		mdsvex({
+			extensions: ['.md'],
+		})
+	],
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
